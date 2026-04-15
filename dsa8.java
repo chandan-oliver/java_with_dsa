9384be11f5a80f3a9a0711730b58e94387e673a7
chandan <ck42377@gmail.com>
2026-04-15 07:47:02 +0530

    loop Question

---
+++ dsa8.java
@@ -0,0 +1,16 @@
+package java_with_dsa;
+// sum of first n natural number
+public class dsa8 {
+    public static void main(String args[]){
+        int n=1;
+        int sum=0;
+        while(n<=5){
+            sum +=n;
+            n++;
+        }
+        System.out.println(sum);
+
+
+    }
+    
+}
