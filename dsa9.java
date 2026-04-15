58e4f61521bd564968e29e814a775710b87acc5a
chandan <ck42377@gmail.com>
2026-04-15 07:50:46 +0530

    loop Question

---
+++ dsa9.java
@@ -0,0 +1,10 @@
+package java_with_dsa;
+// using for loop print 10 times your name
+public class dsa9 {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            System.out.println(" your name");
+        }
+    }
+    
+}
