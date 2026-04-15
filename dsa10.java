d23bb6346372ec01fe268250fd41a671f3ba4b00
chandan <ck42377@gmail.com>
2026-04-15 07:53:41 +0530

    loop Question

---
+++ dsa10.java
@@ -0,0 +1,10 @@
+package java_with_dsa;
+// print pattern using for loop 
+public class dsa10 {
+    public static void main(String args[]){
+        for(int i=1;i<=4;i++){
+            System.out.println("* * * *");
+        }
+    }
+    
+}
