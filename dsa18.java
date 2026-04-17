550038b229481a16a53c5be196528568c19590b6
chandan <ck42377@gmail.com>
2026-04-17 06:49:03 +0530

    loop Question

---
+++ dsa18.java
@@ -0,0 +1,14 @@
+package java_with_dsa;
+// using parameter code
+public class dsa18 {
+    public static void sum(int a,int b){
+        int sum=a+b;
+        System.out.println(sum);
+    }
+    public static void main(String args[]){
+        int a=2;
+        int b=3;
+        sum(a,b);
+    }
+    
+}
