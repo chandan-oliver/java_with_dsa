ffa6a1a2afa637326c26c9e1b990f547ca59dff5
chandan <ck42377@gmail.com>
2026-04-17 06:57:29 +0530

    loop Question

---
+++ dsa22.java
@@ -0,0 +1,17 @@
+package java_with_dsa;
+// factorial of a number
+public class dsa22 {
+    public static int factorial(int n){
+        int f=1;
+        for(int i=1;i<=n;i++){
+            f=f*i;
+
+        }
+        return f;
+    }
+    public static void main(String args[]){
+        int n=5;
+        System.out.println(factorial(n));
+    }
+    
+}
