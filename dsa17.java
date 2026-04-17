aa46eb47748bc06a731a23cfe78a5398cefd24f2
chandan <ck42377@gmail.com>
2026-04-17 06:34:10 +0530

    loop Question

---
+++ dsa17.java
@@ -0,0 +1,16 @@
+package java_with_dsa;
+import java.util.*;
+// int return type
+public class dsa17 {
+    public static int sum(){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        return a+b;
+    }
+    public static void main(String atgs[]){
+        System.out.println(sum());
+
+    }
+    
+}
