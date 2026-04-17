07b32251936965d6e33ae6b73ac5707b2430be0e
chandan <ck42377@gmail.com>
2026-04-17 06:53:49 +0530

    loop Question

---
+++ dsa21.java
@@ -0,0 +1,13 @@
+package java_with_dsa;
+// product of two number
+public class dsa21 {
+    public static int product(int a,int b){
+        return a*b;
+    }
+    public static void main(String args[]){
+        int a=3;
+        int b=4;
+        System.out.println("product of two number "+ product(a,b));
+    }
+    
+}
